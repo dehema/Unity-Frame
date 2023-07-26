d695d9a2bc9f6b7bfa40709703ffc5d0da95078e
dehema <973023578@qq.com>
2023-07-26 17:00:34 +0800

    clear

--- Assets/SOHOShop/Config/SOHOShopConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SOHOShopConfig
-{
-    public static int TaskTotalNum = 30;     // 现金提现任务总数
-
-}
--- Assets/SOHOShop/Model/GoldRedeem.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GoldRedeem : Redeem
-{
-    public string type;    // gold/amazon
-    public int num;        // 提现所需金币
-}
