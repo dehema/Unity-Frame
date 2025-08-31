8fbfafb0cd0f1da9c08f19f2e6f2e8660db04820
孙伯夷 <sunboyi@pwrd.com>
2025-08-31 14:43:09 +0800

    feat: 部署界面

---
+++ Assets/HotUpdateScripts/UI/View/RTS/RTSBattleDeployView/RTSBattleDeployView.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using Rain.UI;
+using UnityEngine;
+
+/// <summary>
+/// ²¿Êð½çÃæ
+/// </summary>
+public partial class RTSBattleDeployView : BaseView
+{
+
+}
