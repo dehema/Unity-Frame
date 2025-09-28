d02d12eae67988b7bc7544a510b5954fd9c62110
孙伯夷 <sunboyi@pwrd.com>
2025-09-28 21:50:39 +0800

    feat: 新增主界面

---
+++ Assets/HotUpdateScripts/UI/View/Common/MainView/MainView.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using Rain.UI;
+using UnityEngine;
+
+/// <summary>
+/// 主UI
+/// </summary>
+public partial class MainView : BaseView
+{
+}
---
+++ Assets/HotUpdateScripts/UI/View/Common/MainView/MainView_UI.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Rain.UI;
+using Rain.Core;
+
+public partial class MainView : Rain.UI.BaseView
+{
+    [System.Serializable]
+    struct SerializableUIComponents
+    {
+        [SerializeField] public GameObject bg;
+        [SerializeField] public Image bg_Image;
+        [SerializeField] public Button bg_Button;
+        [SerializeField] public GameObject content;
+        [SerializeField] public RectTransform content_Rect;
+    }
+
+    [Header("自动序列化组件")]
+    [SerializeField] private SerializableUIComponents ui;
+}
